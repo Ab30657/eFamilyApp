6044b797441f938d25d1e0567a198f6c29488fae
Ab30657 <amar_ronaldo30657@yahoo.com>
2020-10-20 00:29:03 +0545

    Now onto the custom list

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Wpf;
 
 namespace Family
 {
@@ -20,9 +22,26 @@ namespace Family
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        public LineSeries SeriesCollection { get; set; }
+        public string[] Labels { get; set; }
+        public Func<double, string> YFormatter { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+
+            SeriesCollection = new LineSeries
+                {
+                    Title = "Title",
+                    Values = new ChartValues<double> { 400, 6000, 5000, 2000 ,4000 },
+                    PointGeometry= DefaultGeometries.Circle
+            };
+
+            Labels = new[] { "Jan", "Feb", "Mar", "Apr", "May" };
+            YFormatter = value => value.ToString();
+
+            DataContext = this;
         }
     }
 }
---
+++ customcontrols/GroceryViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Family.customcontrols
+{
+    class GroceryViewModel
+    {
+        ResourceDictionary dict = Application.LoadComponent(new Uri("Family;component/resources/images.xaml", UriKind.RelativeOrAbsolute)) as ResourceDictionary;
+
+        public ObservableCollection<GetFileDetails> getFileDetails
+        {
+            get
+            {
+
+                return new ObservableCollection<GetFileDetails>
+                {
+                    new GetFileDetails(){ FileThumb=(BitmapImage)dict["milk"], FileName="Milk", Quantity=2, Unit="litres"}
+                };
+            }
+        }
+    }
+
+    class GetFileDetails
+    {
+        public BitmapImage FileThumb { get; set; }
+
+        public string FileName { get; set; }
+        public string Unit { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
